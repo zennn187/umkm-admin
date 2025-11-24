44af0c9c8eb59c25a192dff5aa45ac322866e843
ozapcr <oza24si@mahasiswa.pcr.ac.id>
2025-11-24 22:28:26 +0700

    Update 2

--- database/migrations/2025_11_10_100327_add_user_columns_to_users_table.php
+++ database/migrations/2025_11_10_100327_add_user_columns_to_users_table.php
@@ -8,18 +8,11 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('phone')->nullable()->after('email');
-            $table->text('alamat')->nullable()->after('phone');
-            $table->string('role')->default('user')->after('alamat');
-            $table->boolean('is_active')->default(true)->after('role');
-        });
+
     }
 
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['phone', 'alamat', 'role', 'is_active']);
-        });
+
     }
 };
